3240d4b4c75310a21878b05b7cf7efec322514a0
WislerC <130715261+WislerC@users.noreply.github.com>
2024-05-05 22:38:05 -0300

    primeiro commit

---
+++ EstruMath.js
@@ -0,0 +1,29 @@
+const productAttributes = [
+    ['P', 'M', 'G'],
+    ['Red', 'Black'],
+    ['For men', 'For women'],
+  ];
+
+  const cartesianProductLong = (...arrays) => {
+    const [arrayA, arrayB, ...rest] = arrays;
+  
+    const result = [];
+    for (let i = 0; i < arrayA.length; i++) {
+      for (let j = 0; j < arrayB.length; j++) {
+        if (Array.isArray(arrayA[i])) {
+          result.push([...arrayA[i], arrayB[j]]);
+        } else {
+          result.push([arrayA[i], arrayB[j]]);
+        }
+      }
+    }
+  
+    if (rest.length > 0) {
+      return cartesianProductLong(result, ...rest);
+    }
+  
+    return result;
+  };
+
+  const result = cartesianProductLong(...productAttributes);
+  console.log(result);
